bea0cb71a79694329ec62b99ffeccadf8b720711
Jason Warta <jasonwarta@gmail.com>
2017-05-12 23:14:04 -0800

    changed path parsing to eliminate directory traversal

--- filehandler.sh
+++ filehandler.sh
@@ -2,7 +2,7 @@
 
 read -r line
 echo "$line" >> requests.txt
-line=$(sed 's,^GET \(.*\) HTTP/1.1.*,\1,'<<<$line)
+line=$(sed 's,^GET \(.*\) HTTP/1.1.*,\1,;s,\.\.,,g'<<<$line)
 
 if [ "$line" == "/" ]; then
 	echo "index.html" >> files.txt
