94f3ae83bc465bfebb0014e750e282bd7ca0330d
Jason Warta <jasonwarta@gmail.com>
2017-05-12 20:59:03 -0800

    modified filehandling to log requests

--- filehandler.sh
+++ filehandler.sh
@@ -1,11 +1,14 @@
 #!/usr/bin/env bash
 
 read -r line
+echo "$line" >> requests.txt
 line=$(sed 's,^GET \(.*\) HTTP/1.1.*,\1,'<<<$line)
 
 if [ "$line" == "/" ]; then
+	echo "index.html" >> files.txt
 	cat index.html
 else
 	fname=".$line"
+	echo "$fname" >> files.txt
 	cat $fname
 fi
